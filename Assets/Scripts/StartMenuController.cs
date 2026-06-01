7a31bf4204c1e5e11d54e63438d736138cd531e3
Phiwe Bhengu <bhengu080104@gmail.com>
2026-06-01 23:46:56 +0200

    Add AudioManager prefab and Master mixer
    Introduce a reusable AudioManager prefab and Master audio mixer (with exposed MusicVolume and SFXVolume parameters). Update SampleScene to replace embedded audio GameObjects with a PrefabInstance of the new AudioManager, and update scene roots accordingly. Also add a small UI 'Fill' GameObject to StartMenu and modify StartMenuController.cs (script changes included) to integrate with the new audio/UI setup. This centralizes audio configuration and makes audio objects reusable across scenes.

--- Assets/Scripts/StartMenuController.cs
+++ Assets/Scripts/StartMenuController.cs
@@ -1,16 +1,24 @@
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
+using UnityEngine.Audio;
+using UnityEngine.UI;
 
 public class StartMenuController : MonoBehaviour
 {
+    [Header("Audio")]
+    public AudioMixer audioMixer;
+    public Slider musicSlider;
+    public Slider sfxSlider;
+
     void Start()
     {
+        // Play menu music
         if (MusicManager.Instance != null)
         {
             MusicManager.Instance.PlayMusic("MainMenu");
         }
+
+        LoadVolume();
     }
 
     public void OnStartClick()
@@ -24,5 +32,43 @@ public class StartMenuController : MonoBehaviour
         UnityEditor.EditorApplication.isPlaying = false;
 #endif
         Application.Quit();
+    }
+
+    // =========================
+    // VOLUME CONTROL
+    // =========================
+
+    public void UpdateMusicVolume(float volume)
+    {
+        audioMixer.SetFloat("MusicVolume", volume);
+    }
+
+    public void UpdateSoundVolume(float volume)
+    {
+        audioMixer.SetFloat("SFXVolume", volume);
+    }
+
+    public void SaveVolume()
+    {
+        if (audioMixer.GetFloat("MusicVolume", out float musicVolume))
+            PlayerPrefs.SetFloat("MusicVolume", musicVolume);
+
+        if (audioMixer.GetFloat("SFXVolume", out float sfxVolume))
+            PlayerPrefs.SetFloat("SFXVolume", sfxVolume);
+    }
+
+    public void LoadVolume()
+    {
+        float musicVol = PlayerPrefs.GetFloat("MusicVolume", 0f);
+        float sfxVol = PlayerPrefs.GetFloat("SFXVolume", 0f);
+
+        if (musicSlider != null)
+            musicSlider.value = musicVol;
+
+        if (sfxSlider != null)
+            sfxSlider.value = sfxVol;
+
+        audioMixer.SetFloat("MusicVolume", musicVol);
+        audioMixer.SetFloat("SFXVolume", sfxVol);
     }
 }
